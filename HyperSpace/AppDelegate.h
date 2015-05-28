85b79d5ad226e756f06cd494de0536bb65349bcb
callum taylor <razzile@callums-Mac.local>
2015-05-28 17:58:01 +0100

    Initial Commit

---
+++ HyperSpace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HyperSpace
+//
+//  Created by callum taylor on 28/05/2015.
+//  Copyright (c) 2015 Satori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HyperSpace/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HyperSpace
+//
+//  Created by callum taylor on 28/05/2015.
+//  Copyright (c) 2015 Satori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
